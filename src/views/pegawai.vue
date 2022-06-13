c78c9ba2810055530fbc78f15fa34229b9089375
Agung Mulia <snubsgungs@gmail.com>
2022-06-13 21:34:00 +0700

    make it better

--- src/views/pegawai.vue
+++ src/views/pegawai.vue
@@ -140,11 +140,7 @@ export default {
     }
   },
   computed: {
-    searchPegawai: function(){
-    return this.store.state.pegawai.data.filter((value) => {
-      return value.NAMA_PEGAWAI.match(this.search);
-      });
-    }
+    
   }
 }
 </script>
